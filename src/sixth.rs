0de12fe143276089e42566a84fd3449f1b7f0d66
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-04-03 15:07:47 -0300

    feat(sixth): Iterators and peek

--- src/sixth.rs
+++ src/sixth.rs
@@ -61,11 +61,89 @@ impl<T> List<T> {
       }
     }
   }
+
+  pub fn peek(&self) -> Option<&T> {
+    unsafe { self.head.as_ref().map(|node| &node.elem) }
+  }
+
+  pub fn peek_mut(&mut self) -> Option<&mut T> {
+    unsafe { self.head.as_mut().map(|node| &mut node.elem) }
+  }
+
+  pub fn into_iter(self) -> IntoIter<T> {
+    IntoIter(self)
+  }
+
+  pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+    Iter {
+      next: if self.head.is_null() {
+        None
+      } else {
+        unsafe { Some(&*self.head) }
+      },
+    }
+  }
+
+  pub fn iter_mut<'a>(&'a mut self) -> IterMut<'a, T> {
+    IterMut {
+      next: if self.head.is_null() {
+        None
+      } else {
+        unsafe { Some(&mut *self.head) }
+      },
+    }
+  }
 }
 
 impl<T> Drop for List<T> {
   fn drop(&mut self) {
     while self.pop().is_some() {}
+  }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.0.pop()
+  }
+}
+
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    let node = self.next.take()?;
+    self.next = if node.next.is_null() {
+      None
+    } else {
+      unsafe { Some(&*node.next) }
+    };
+    Some(&node.elem)
+  }
+}
+
+pub struct IterMut<'a, T> {
+  next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+  type Item = &'a mut T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    let node = self.next.take()?;
+    self.next = if node.next.is_null() {
+      None
+    } else {
+      unsafe { Some(&mut *node.next) }
+    };
+    Some(&mut node.elem)
   }
 }
 
@@ -100,5 +178,43 @@ mod tests {
     assert_eq!(Some(6), list.pop());
     assert_eq!(Some(7), list.pop());
     assert_eq!(None, list.pop());
+  }
+
+  #[test]
+  fn sixth_miri_food() {
+    let mut list = List::new();
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    assert!(list.pop() == Some(1));
+    list.push(4);
+    assert!(list.pop() == Some(2));
+    list.push(5);
+
+    assert!(list.peek() == Some(&3));
+    list.push(6);
+    list.peek_mut().map(|x| *x *= 10);
+    assert!(list.peek() == Some(&30));
+    assert!(list.pop() == Some(30));
+
+    for elem in list.iter_mut() {
+      *elem *= 100;
+    }
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&400));
+    assert_eq!(iter.next(), Some(&500));
+    assert_eq!(iter.next(), Some(&600));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.next(), None);
+
+    assert!(list.pop() == Some(400));
+    list.peek_mut().map(|x| *x *= 10);
+    assert!(list.peek() == Some(&5000));
+    list.push(7);
+
+    // Drop it on the ground and let the dtor exercise itself
   }
 }
